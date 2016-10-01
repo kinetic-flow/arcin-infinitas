d552fd03d26a6d81d124d7c2d0f813af9eb3d915
Bryan Donlan <bdonlan@gmail.com>
2016-10-01 21:38:58 +0000

    Cherry-pick 6d752bf581dd8959b13a22d2aa04d5efbdda5693 from bdonlan/arcin

--- arcin/main.cpp
+++ arcin/main.cpp
@@ -350,6 +350,67 @@ HID_arcin usb_hid(usb, report_desc_p);
 
 USB_strings usb_strings(usb, config.label);
 
+class analog_button {
+	public:
+		// config
+
+		// Number of ticks we need to advance before recognizing an input
+		uint32_t deadzone;
+		// How long to sustain the input before clearing it (if opposite direction is input, we'll release immediately)
+		uint32_t sustain_ms;
+		// Always provide a zero-input for one poll before reversing?
+		bool clear;
+
+		const volatile uint32_t &counter;
+
+		// State: Center of deadzone
+		uint32_t center;
+		// times to: reset to zero, reset center to counter
+		uint32_t t_timeout;
+
+		int8_t state; // -1, 0, 1
+		int8_t last_delta;
+	public:
+		analog_button(volatile uint32_t &counter, uint32_t deadzone, uint32_t sustain_ms, bool clear)
+			: deadzone(deadzone), sustain_ms(sustain_ms), clear(clear), counter(counter)
+		{
+			center = counter;
+			t_timeout = 0;
+			state = 0;
+		}
+
+		int8_t poll() {
+			uint8_t observed = counter;
+			int8_t delta = observed - center;
+			last_delta = delta;
+
+			uint8_t direction = 0;
+			if (delta >= (int32_t)deadzone) {
+				direction = 1;
+			} else if (delta <= -(int32_t)deadzone) {
+				direction = -1;
+			}
+
+			if (direction != 0) {
+				center = observed;
+				t_timeout = Time::time() + sustain_ms;
+			} else if (t_timeout != 0 && Time::time() >= t_timeout) {
+				state = 0;
+				center = observed;
+				t_timeout = 0;
+			}
+
+			if (direction == -state && clear) {
+				state = direction;
+				return 0;
+			} else if (direction != 0) {
+				state = direction;
+			}
+
+			return state;
+		}
+};
+
 int main() {
     rcc_init();
     
