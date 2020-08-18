3942cd0a6214b23130d133ebb6fd698921b46957
minsang <54884351+minsang-github@users.noreply.github.com>
2020-08-17 20:36:34 -0700

    fix e2 function

--- arcin/multifunc.cpp
+++ arcin/multifunc.cpp
@@ -13,19 +13,11 @@ static uint32_t first_e2_rising_edge_time = 0;
 static uint32_t e2_rising_edge_count = 0;
 
 static bool last_e2_status = false;
-static uint32_t last_update_time = 0;
 void capture_e2_presses(bool pressed) {
-    // Update at most once per 1ms.
-    if (Time::time() == last_update_time) {
-        return;
-    }
-
-    last_update_time = Time::time();
-
     // rising edge (detect off-on sequence)
     if (!last_e2_status && pressed) {
         if (first_e2_rising_edge_time == 0) {
-            first_e2_rising_edge_time = last_update_time;
+            first_e2_rising_edge_time = Time::time();
         }
 
         e2_rising_edge_count += 1;
@@ -74,33 +66,43 @@ bool is_capture_window_past_due() {
     return diff > MULTITAP_DETECTION_WINDOW_MS;
 }
 
+#define EFFECTOR_COMBO_HOLD_DURATION_MS 100
+
+static uint32_t last_update_time = 0;
 static uint16_t effector_buttons_being_asserted = 0;
 static uint32_t effector_buttons_assertion_start = 0;
 uint16_t get_multi_function_keys(bool is_e2_pressed) {
-    if (effector_buttons_assertion_start == 0) {
+    // Update at most once per 1ms. Otherwise, just return the last calculated
+    // result.
+    if (Time::time() == last_update_time) {
+        return effector_buttons_being_asserted;
+    }
+    last_update_time = Time::time();
+
+    if (effector_buttons_being_asserted == 0) {
         capture_e2_presses(is_e2_pressed);
     }
 
-    if (is_e2_pressed && (effector_buttons_assertion_start > 0)) {
-        // Keep the time rolling as long as the button is being pressed down
-        effector_buttons_assertion_start = Time::time();
-    }
-
     if (is_capture_window_past_due()) {
+        // Start asserting button combo
         effector_buttons_being_asserted = get_multitap_output();
+        effector_buttons_assertion_start = Time::time();
         
         // Reset
         first_e2_rising_edge_time = 0;
         e2_rising_edge_count = 0;
-
-        // Start asserting button combo
-        effector_buttons_assertion_start = Time::time();
     }
-    
+
     if (effector_buttons_assertion_start > 0 &&
-        (Time::time() - effector_buttons_assertion_start) >= 100) {
-        effector_buttons_being_asserted = 0;
-        effector_buttons_assertion_start = 0;
+        (Time::time() - effector_buttons_assertion_start) >= EFFECTOR_COMBO_HOLD_DURATION_MS) {
+
+        if (is_e2_pressed) {
+            // Extend the time since the button is still pressed
+            effector_buttons_assertion_start = Time::time() - (EFFECTOR_COMBO_HOLD_DURATION_MS - 10);
+        } else {
+            effector_buttons_being_asserted = 0;
+            effector_buttons_assertion_start = 0;
+        }
     }
 
     return effector_buttons_being_asserted;
