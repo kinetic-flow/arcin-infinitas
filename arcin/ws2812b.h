3444e8c6a4d4e588ceec4e2a0b6ab990e960f40f
Minsang <54884351+minsang-github@users.noreply.github.com>
2020-12-23 23:20:48 -0800

    pride 2015

--- arcin/ws2812b.h
+++ arcin/ws2812b.h
@@ -68,6 +68,7 @@ typedef enum _WS2812B_Mode {
     // animated - reverse sawtooth (flash all with random color, fade out to 0, repeat)
     // tt       - tt movement turns all LED to a new random color
     WS2812B_MODE_RANDOM_HUE,
+    WS2812B_MODE_PRIDE,
 } WS2812B_Mode;
 
 typedef enum _WS2812B_Palette {
@@ -211,6 +212,8 @@ class WS2812B {
             }
         }
 };
+
+void animation_pride_2015(WS2812B& ws2812b);
 
 class RGBManager {
 
@@ -693,6 +696,13 @@ class RGBManager {
                 }
                 break;
 
+                case WS2812B_MODE_PRIDE:
+                {
+                    animation_pride_2015(ws2812b);
+                    this->show();
+                }
+                break;
+
                 case WS2812B_MODE_SINGLE_COLOR:
                 default:
                 {
@@ -719,5 +729,49 @@ class RGBManager {
             ws2812b.irq();
         }
 };
+
+// from FastLED example:
+// Pride2015
+// Animated, ever-changing rainbows.
+// by Mark Kriegsman
+void animation_pride_2015(WS2812B& ws2812b) {
+    static uint16_t sPseudotime = 0;
+    static uint16_t sLastMillis = 0;
+    static uint16_t sHue16 = 0;
+
+    uint8_t sat8 = beatsin88( 87, 220, 250);
+    uint8_t brightdepth = beatsin88( 341, 96, 224);
+    uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+    uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+    uint16_t hue16 = sHue16;//gHue * 256;
+    uint16_t hueinc16 = beatsin88(113, 1, 3000);
+
+    uint16_t ms = get_millisecond_timer();
+    uint16_t deltams = ms - sLastMillis ;
+    sLastMillis  = ms;
+    sPseudotime += deltams * msmultiplier;
+    sHue16 += deltams * beatsin88( 400, 5,9);
+    uint16_t brightnesstheta16 = sPseudotime;
+
+    for( uint16_t i = 0 ; i < ws2812b.get_num_leds(); i++) {
+        hue16 += hueinc16;
+        uint8_t hue8 = hue16 / 256;
+
+        brightnesstheta16  += brightnessthetainc16;
+        uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+        uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+        uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+        bri8 += (255 - brightdepth);
+
+        CRGB newcolor = CHSV( hue8, sat8, bri8);
+
+        uint16_t pixelnumber = i;
+        pixelnumber = (ws2812b.get_num_leds()-1) - pixelnumber;
+
+        nblend( ws2812b.leds[pixelnumber], newcolor, 64);
+    }
+}
 
 #endif
